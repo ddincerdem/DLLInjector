8c3a3b141572eb729b7ff9577ca9f8f7c14e80f4
Doğan Dinçer DEMİRCİ <47913600+ddincerdem@users.noreply.github.com>
2021-10-04 13:26:55 +0300

    Source Code
    Basic DLL Injection kodunun eklenmesi yapıldı.

---
+++ DLLFormApplication/DLLFormApplication/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace DLLFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PNameT = new System.Windows.Forms.TextBox();
+            this.DllPathT = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(10, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(50, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DLL Path";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(191, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(119, 95);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // PNameT
+            // 
+            this.PNameT.Location = new System.Drawing.Point(170, 21);
+            this.PNameT.Name = "PNameT";
+            this.PNameT.Size = new System.Drawing.Size(209, 20);
+            this.PNameT.TabIndex = 3;
+            // 
+            // DllPathT
+            // 
+            this.DllPathT.Location = new System.Drawing.Point(170, 58);
+            this.DllPathT.Name = "DllPathT";
+            this.DllPathT.Size = new System.Drawing.Size(209, 20);
+            this.DllPathT.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(385, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = ". . .";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Show Procesess";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(329, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Inject";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(458, 214);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DllPathT);
+            this.Controls.Add(this.PNameT);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DLL Injector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox PNameT;
+        private System.Windows.Forms.TextBox DllPathT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ DLLFormApplication/DLLFormApplication/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+
+namespace DLLFormApplication
+{
+    public partial class Form1 : Form
+    {
+        //static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+
+        //[DllImport("kernel32.dll", SetLastError = true)] 
+        //static extern IntPtr OpenProcess(uint dwDesiredAcces, int bInheritHandle, uint dwProcessId);
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern int CloseHandle(IntPtr hObject);
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern IntPtr GetProcAddress(IntPtr Hmodule , string lpProcName);
+
+        //[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        //public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwsize, uint flAllocationType, uint flProtect);
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddres, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddres, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId );
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess,bool bInheritHandle,int processId);
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess(flags, false, proc.Id);
+        }
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, AllocationType dwFreeType);
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess,IntPtr lpAddress,IntPtr dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess,IntPtr lpBaseAddress,[MarshalAs(UnmanagedType.AsAny)] object lpBuffer,int dwSize,out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes,uint dwStackSize, IntPtr lpStartAddress,IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Olası sonuçlar listesi
+        public enum DllInjectionResult
+        {
+            DllNotFound,
+            ProcessNotFound,
+            InjectionFailed,
+            Succes
+        }
+
+        //Sonuçlarıyla birlikte DLL injectionu gerçekleştiren fonksiyon 
+        public DllInjectionResult Inject(String sProcName, string sDLLPath)
+        {
+            if(!File.Exists(sDLLPath))
+            {
+                MessageBox.Show("8 Numaralı hata");
+
+                return DllInjectionResult.DllNotFound;
+
+            }
+
+            uint _procId = 0;
+
+            Process[] _procs = Process.GetProcesses();
+
+            for (int i = 0; i < _procs.Length; i++)
+            {
+                if (_procs[i].ProcessName == sProcName)
+                {
+                    _procId = (uint)_procs[i].Id;
+                    break;
+                }
+            }
+
+            if (_procId == 0)
+            {
+                MessageBox.Show("9 Numaralı hata");
+
+                return DllInjectionResult.ProcessNotFound;
+            }
+
+            if (!bInject(_procId, sDLLPath))
+            {
+                MessageBox.Show("10 Numaralı hata");
+                return DllInjectionResult.InjectionFailed;
+            }
+
+            MessageBox.Show("11 Numaralı Başarı");
+            return DllInjectionResult.Succes;
+        }
+
+        //Enjekte eden program 
+        bool bInject(uint pToBeInject, string DllPath)
+        {
+            //unsigned olacak
+            int ProcId = (int)pToBeInject;
+            IntPtr Size = (IntPtr)DllPath.Length;
+
+            if (!File.Exists(DllPath))
+            {
+                MessageBox.Show("Böyle bir DLL yok!");
+                return false;
+            }
+            try
+            {
+                Process localById = Process.GetProcessById(ProcId);
+                string ProcName = localById.ProcessName;
+            }
+            catch (ArgumentException e)
+            {
+                MessageBox.Show("7 Numaralı hata");
+                return false;
+            }
+
+            // OpenProcess ile ProcID ve gerekli izinler ile kullanılacak process seçilir 
+
+            IntPtr ProcHandle = OpenProcess( ProcessAccessFlags.All,false,ProcId);
+
+            if (ProcHandle == null)
+            {
+                MessageBox.Show("1 Numaralı hata");
+                return false;
+            }
+
+            // Victim Process'in içerisinin sanal adresinde yer ayıran fonksiyon
+
+            IntPtr DllSpace = VirtualAllocEx(ProcHandle,IntPtr.Zero,Size,AllocationType.Reserve | AllocationType.Commit,MemoryProtection.ExecuteReadWrite);
+
+            if (DllSpace == null)
+            {
+                MessageBox.Show("2 Numaralı hata");
+                return false;
+            }
+           
+            //Ele alınan process'in ayrılan bellek alanına enjekte edilecel DLL dosyasının yolunu yazmak için WriteProcessMemory() kullanılır.
+
+            byte[] bytes = Encoding.ASCII.GetBytes(DllPath);
+            bool DllWrite = WriteProcessMemory(ProcHandle,DllSpace,bytes, (int)bytes.Length,out var bytesread);
+
+            if (!DllWrite)
+            {
+                MessageBox.Show("3 Numaralı hata");
+                return false;
+            }
+
+            //GetModuleHandle fonksiyonu ile ele alıncak DLL kütüphanesi belirlenir ve içeride çalıştırılacak LoadLibraryA() fonksiyonunun adresi GetProcAdress ile alınır.
+
+            IntPtr Kernel32Handle = GetModuleHandle("Kernel32.dll");
+            IntPtr LoadLibraryAAddress = GetProcAddress(Kernel32Handle, "LoadLibraryA");
+
+            if (LoadLibraryAAddress == null)
+            {
+                MessageBox.Show("4 Numaralı hata");
+                return false;
+            }
+
+            //CreateRemoteThread() fonksiyonu bir processin sanal bellek alanında thread oluşturur. Bu fonksiyon sayesinde LoadLibraryA() fonksiyonu processin ayrılan 
+            //kısmında bir thread olarak çalıştırılır. Böylece DLL enjekte edilmiş olur.
+            //!!!!!!!!!!!!!!!!!!!
+
+            IntPtr RemoteThreadHandle;
+            try
+            {
+              RemoteThreadHandle = CreateRemoteThread(ProcHandle, IntPtr.Zero, 0, LoadLibraryAAddress, DllSpace, 0, IntPtr.Zero);
+            }
+            catch (Exception)
+            {
+                RemoteThreadHandle = IntPtr.Zero;
+                throw;
+            }
+
+            if (RemoteThreadHandle == null)
+            {
+                MessageBox.Show("5 Numaralı hata");
+                return false;
+            }
+
+            // Deallocate memory assigned to DLL
+            bool FreeDllSpace = VirtualFreeEx(ProcHandle,DllSpace,0,AllocationType.Release);
+
+            if (FreeDllSpace == false)
+            {
+                MessageBox.Show("6 Numaralı hata");
+                return false;
+            }
+
+            // Close remote thread handle
+            CloseHandle(RemoteThreadHandle);
+
+            // Close target process handle
+            CloseHandle(ProcHandle);
+            return true;
+        }
+
+        //Injection başamadan önceki ara katman 
+        private void InjectMode()
+        {
+                Process[] ProcessCollection = Process.GetProcesses();
+                
+                foreach (Process p in ProcessCollection)
+                {
+                    if (p.ProcessName == PNameT.Text.ToString())
+                    {
+                        Inject(PNameT.Text, DllPathT.Text);
+                        break;
+                    }
+                }
+           
+        }
+
+        //ListBoxtan seçim yapıp TextBox'a yazdırmaya yarıyor
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count != 0 )
+            {
+                PNameT.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+       
+        //Injectionu başlatıyor 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            InjectMode();
+        }
+        
+        //DLL'leri Listeliyor
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                Process p = processes[i];
+                listBox1.Items.Add(p.ProcessName);
+            }
+        }
+
+        //DLL seçtiren Open Dialogu açıyor 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DllPathT.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
